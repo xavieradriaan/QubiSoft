8cfc1180e8d35d7fbfa052ee403b558a8a172c39
Xavier Andrade <xavierandrade@MacBook-Pro-de-Xavier.local>
2025-05-02 13:41:01 -0500

    nuevo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -38,7 +38,7 @@ const Footer = () => {
         <div className="footer-section">
           <h4 className="section-title">Contacto</h4>
           <div className="contact-container">
-            <a href="mailto:contact@Qubitsoft.io" className="email-link">
+            <a href="mailto:contact@Qubitsoft.net" className="email-link">
               <svg className="icon" viewBox="0 0 24 24">
                 <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 14H4V8l8 5 8-5v10zm-8-7L4 6h16l-8 5z"/>
               </svg>
